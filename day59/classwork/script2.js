1f3f926328f20a2151288775aa8f0a99f528e141
NikoMose <n74648439@gmail.com>
2026-04-04 11:06:03 +0400

    homework

---
+++ day59/classwork/script2.js
@@ -0,0 +1,14 @@
+let country = "Georgia";
+let city = "Tbilisi";
+
+let both = country + " " + city;
+
+console.log(both);
+
+//-------------------------
+
+let text = "Hello, my name is niko";
+
+console.log(text.length);
+
+// var არის ძველი keyword რომელსაც არავინ ხმარობს, ის არსერბობს რადგან ძველი საიტები იყენებენ, constს ვიყენებთ როდესაც მნიშვნელობის შეცვლას არ ვაპირებთ
---
+++ day59/homewrok/script.js
@@ -0,0 +1,29 @@
+let a = 5 + 3;
+let b = 10 + 7;
+let c = 20 + 15;
+let d = 8 + 12;
+let e = 100 + 50;
+
+let f = 10 - 4;
+let g = 25 - 10;
+let h = 50 - 30;
+let i = 100 - 45;
+let j = 70 - 20;
+
+let k = 3 * 4;
+let l = 5 * 6;
+let m = 7 * 8;
+let n = 9 * 3;
+let o = 10 * 10;
+
+let p = 20 / 4;
+let q = 100 / 5;
+let r = 45 / 9;
+let s = 60 / 3;
+let t = 144 / 12;
+
+let u = 10 % 3;
+let v = 25 % 4;
+let w = 18 % 5;
+let x = 50 % 7;
+let y = 100 % 9;
